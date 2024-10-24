980a92ad2473edd566ae3790738fcb0824f71488
Gabriel da Cunha <gabriel.cunhagabi@gmail.com>
2024-10-23 23:28:40 -0300

    Desenvolvendo CRUD da raça

--- app/racas.php
+++ app/racas.php
@@ -52,7 +52,7 @@
                   </div>
                   
                   <div class="col-3" align="right">
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#novoUsuarioModal">
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#novaRacaModal">
                       Nova Raça
                     </button>
                   </div>
@@ -94,17 +94,17 @@
       </div>
       <!-- /.container-fluid -->
 
-      <div class="modal fade" id="novoUsuarioModal">
+      <div class="modal fade" id="novaRacaModal">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header bg-success">
-              <h4 class="modal-title">Novo Usuário</h4>
+              <h4 class="modal-title">Nova Raça</h4>
               <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              <form method="POST" action="php/salvarUsuario.php?funcao=I" enctype="multipart/form-data">              
+              <form method="POST" action="php/salvarRaca.php?funcao=I" enctype="multipart/form-data">              
                 
                 <div class="row">
                   <div class="col-8">
@@ -123,84 +123,15 @@
                       </select>
                     </div>
                   </div>
-
-                  <div class="col-8">
-                    <div class="form-group">
-                      <label for="iLogin">Login:</label>
-                      <input type="email" class="form-control" id="iLogin" name="nLogin" maxlength="50">
-                    </div>
-                  </div>
-
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label for="iSenha">Senha:</label>
-                      <input type="text" class="form-control" id="iSenha" name="nSenha" maxlength="6">
-                    </div>
-                  </div>
                 
                   <div class="col-12">
                     <div class="form-group">
-                      <label for="iFoto">Foto:</label>
-                      <input type="file" class="form-control" id="iFoto" name="Foto" accept="image/*">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="iFoto" name="Foto" accept="image/*">
+                        <label class="custom-file-label" for="customFile">Escolher imagem</label>
+                      </div>
                     </div>
                   </div>
-                
-                  <div class="col-12">
-                    <div class="form-group">
-                      <input type="checkbox" id="iAtivo" name="nAtivo">
-                      <label for="iAtivo">Usuário Ativo</label>
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <div class="form-group">
-                      <label>CEP</label>
-                      <input required name="CEP" type="text" class="form-control cep">
-                    </div>
-                  </div>
-                  
-                  <div class="col-9">
-                    <div class="form-group">
-                      <label>Endereço</label>
-                      <input required name="Endereco" type="text" class="form-control">
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <div class="form-group">
-                      <label>Número</label>
-                      <input required name="Numero" type="text" maxlength="8" class="form-control">
-                    </div>
-                  </div>
-
-                  <div class="col-9">
-                    <div class="form-group">
-                      <label>Complemento</label>
-                      <input name="Complemento" type="text" maxlength="50" class="form-control">
-                    </div>
-                  </div>
-
-                  <div class="col-5">
-                    <div class="form-group">
-                      <label>Bairro</label>
-                      <input required name="Bairro" type="text" class="form-control">
-                    </div>
-                  </div>
-                  
-                  <div class="col-5">
-                    <div class="form-group">
-                      <label>Cidade</label>
-                      <input required name="Cidade" type="text" class="form-control">
-                    </div>
-                  </div>
-
-                  <div class="col-2">
-                    <div class="form-group">
-                      <label>UF</label>
-                      <input required name="UF" type="text" class="form-control">
-                    </div>
-                  </div>
-
                 </div>
 
                 <div class="modal-footer">
