bb99c30f40b73dc92b14d3f6da2721935a4cd874
Gabriel da Cunha <gabriel.cunhagabi@gmail.com>
2024-11-06 22:13:10 -0300

    Ajustes no CRUD de Serviços

--- app/php/salvarServico.php
+++ app/php/salvarServico.php
@@ -2,14 +2,20 @@
 
     include('funcoes.php');
 
-    $nome        = $_POST["nTitulo"   ];
-    $valor       = $_POST["nValor"    ];
-    $duracao     = $_POST["nDuracao"  ];
-    $descricao   = $_POST["nDescricao"];
-    $funcao      = $_GET ["funcao"    ];
-    $idServico   = $_GET ["codigo"    ];
+    $nome           = $_POST["nTitulo"        ];
+    $valorPequeno   = $_POST["nValorPequeno"  ];
+    $duracaoPequeno = $_POST["nDuracaoPequeno"];
+    $valorMedio     = $_POST["nValorMedio"    ];
+    $duracaoMedio   = $_POST["nDuracaoMedio"  ];
+    $valorGrande    = $_POST["nValorGrande"   ];
+    $duracaoGrande  = $_POST["nDuracaoGrande" ];
+    $descricao      = $_POST["nDescricao"     ];
+    $funcao         = $_GET ["funcao"         ];
+    $idServico      = $_GET ["codigo"         ];
 
-    $valor = desformatarMoeda($valor);
+    $valorPequeno = desformatarMoeda($valorPequeno);
+    $valorMedio   = desformatarMoeda($valorMedio);
+    $valorGrande  = desformatarMoeda($valorGrande);
 
     if($_POST["nAtivoServico"] == "on") $ativo = "1"; else $ativo = "0";
 
@@ -19,18 +25,22 @@
     if($funcao == "I"){
 
         //INSERT
-        $sql = "INSERT INTO servico (nome,valor,duracao,descricao,ativo) "
-             ." VALUES ("."'$nome',$valor,'$duracao','$descricao',$ativo);";
+        $sql = "INSERT INTO servico (nome,valor_pequeno,valor_medio,valor_grande,duracao_pequeno,duracao_medio,duracao_grande,descricao,ativo) "
+             ." VALUES ("."'$nome',$valorPequeno,$valorMedio,$valorGrande,'$duracaoPequeno','$duracaoMedio','$duracaoGrande','$descricao',$ativo);";
 
     }elseif($funcao == "A"){
         
         $sql = "UPDATE servico "
-                ." SET nome         = '$nome', "
-                    ." valor        = $valor, "
-                    ." duracao      = '$duracao', "
-                    ." descricao    = '$descricao', "
-                    ." ativo        = $ativo "
-                ." WHERE id_servico = $idServico;";
+                ." SET nome            = '$nome', "
+                    ." valor_pequeno   = $valorPequeno, " 
+                    ." valor_medio     = $valorMedio, " 
+                    ." valor_grande    = $valorGrande, " 
+                    ." duracao_pequeno = '$duracaoPequeno', "
+                    ." duracao_medio   = '$duracaoMedio', "
+                    ." duracao_grande  = '$duracaoGrande', "
+                    ." descricao       = '$descricao', "
+                    ." ativo           = $ativo "
+                ." WHERE id_servico    = $idServico;";
 
     }elseif($funcao == "D"){
         //DELETE
