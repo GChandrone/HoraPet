ad180913a22dad75b0f3c269f2b0607ae4bc69c8
Jean <jeannackoliver@gmail.com>
2024-11-28 20:26:42 -0300

    login e site

--- app/index.php
+++ app/index.php
@@ -1,34 +1,101 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
-    <head>
-		
-		<meta charset="UTF-8">
-        <title>PHP</title>
 
-        <!-- CSS -->
-        <?php include('partes/css.php'); ?>
-        <!-- Fim CSS -->
+<head>
 
-    </head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
 
-    <body class="tela-login">
+    <!-- CSS -->
+    <?php include('partes/css.php'); ?>
+    <!-- Fim CSS -->
 
-        <form method="POST" action="php/validaLogin.php">
-        
-            <p>
-                <label for="iEmail">E-mail: </label>
-                <input type="email" id="iEmail" name="nEmail" required>
-            </p>
 
-            <p>
-                <label for="iSenha">Senha: </label>
-                <input type="password" id="iSenha" name="nSenha" required>
-            </p>
+</head>
 
-            <button type="submit">Logar</button>
+<body class="img js-fullheight" style="background-image: url(dist/img/backiee-103563.jpg);">
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5"></div>
+            </div>
 
-        </form>
+            <form method="POST" action="php/validaLogin.php">
 
-    </body>
+
+                        <div class="TelaDeLogin">
+                            <div class="wrapper">
+                                <div class="card-switch">
+                                    <div class="flip-card__inner">
+                                        <!-- Tela de Login -->
+                                        <div class="flip-card__front">
+                                            <div class="row justify-content-center">
+                                                <div class="col-md-6 col-lg-4">
+                                                    <div class="login-wrap p-0">
+                                                        <div class="fundotransp">
+                                                            <h3 class="mb-4 text-center">Seja bem-vindo</h3>
+                                                            <form action="#" class="signin-form">
+                                                                <div class="form-group">
+                                                                    <input type="email" id="iEmail" name="nEmail"
+                                                                        class="form-control" placeholder="Email" required>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <input id="password-field" type="password" id="iSenha"
+                                                                        name="nSenha" class="form-control" placeholder="Senha"
+                                                                        required>
+                                                                    <span toggle="#password-field"
+                                                                        class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                                                </div>
+                                                                <!-- Botão para enviar o formulário de login -->
+                                                                <div class="form-group">
+                                                                    
+                                                                    <button type="submit"
+                                                                        class="form-control btn btn-primary submit px-3"
+                                                                        id="loginButton">Entrar</button>
+                                                                </div>
+
+                                                                <!-- Botão para virar o card para cadastro -->
+                                                                <button id="flipButton"
+                                                                    class="btn btn-primary">Esqueceu a senha</button>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                
+                                        
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                        </section>
+
+                        <script src="js/jquery.min.js"></script>
+                        <script src="js/popper.js"></script>
+                        <script src="js/bootstrap.min.js"></script>
+                        <script src="js/main.js"></script>
+
+                        <script>
+                            // JavaScript para controlar a rotação do card
+                            const flipButton = document.getElementById('flipButton');
+                            const backButton = document.getElementById('backButton');
+                            const flipCardInner = document.querySelector('.flip-card__inner');
+
+                            flipButton.addEventListener('click', () => {
+                                flipCardInner.classList.add('flipped'); // Ativa rotação para cadastro
+                            });
+
+                            backButton.addEventListener('click', () => {
+                                flipCardInner.classList.remove('flipped'); // Remove a rotação e volta para login
+                            });
+                        </script>
+
+    </form>
+
+</body>
 
 </html>
--- app/partes/css.php
+++ app/partes/css.php
@@ -36,4 +36,9 @@
   <!-- CSS Novo -->  
   <link rel="stylesheet" href="dist/css/style.css">
 
+  <!-- CSS login -->  
+  <link rel="stylesheet" href="dist/css/stylelogin.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
